206d4a29e49c4dadaa6127d2d73ee4cebe4397a3
duan <wenxueduan@gmail.com>
2020-01-10 21:49:12 +0800

    first commit

---
+++ model/src/parse/XMLParser.java
@@ -0,0 +1,10 @@
+package parse;
+
+import org.dom4j.io.SAXReader;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.Element;
+
+public class XMLParser {
+}
